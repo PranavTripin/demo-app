ae86a6537fa5d7d65d46452361946a6b5af249ad
Pranav Anand <pranav.anand@tripin.co.in>
2017-07-04 18:10:02 +0530

    1st commit

---
+++ server.js
@@ -0,0 +1,8 @@
+ var express = require('express');
+   var port = 8087;
+   var app = express();
+   app.get('/', function (req, res) {
+     res.send('You just deployed some Node!\n');
+   });
+   app.listen(port);
+   console.log('Running on http://localhost:' + port);
