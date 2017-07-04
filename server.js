9a260524cbe1427c1996b4e6f8cac643978ed4d0
Pranav Anand <pranav.anand@tripin.co.in>
2017-07-04 18:14:53 +0530

    first push

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
- var express = require('express');
-   var port = 8087;
-   var app = express();
-   app.get('/', function (req, res) {
-     res.send('You just deployed some Node!\n');
-   });
-   app.listen(port);
-   console.log('Running on http://localhost:' + port);
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res) {
+  res.send("Hello World");
+});
+
+app.listen(4000);
